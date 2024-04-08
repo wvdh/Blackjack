684a8518401f94868308cbd53a24f3c745145512
wvdh <wanda@vandenhoogen.eu>
2024-04-08 21:10:46 +0200

    initial setup

---
+++ index.js
@@ -0,0 +1,39 @@
+
+let firstCard = 10
+let secondCard = 4
+let cards = [firstCard, secondCard] // array - ordered list of items
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let messageEl = document.getElementById("message-el");
+let message = ""
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function startGame() {
+    renderGame()    
+}
+
+function renderGame() {
+    // render out firstCard and secondCard
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    // render out ALL the cards we have
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message;
+}
+
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+    let card = 6
+    sum += card 
+    renderGame()
+}
