cbce9fbf719d0d144695c1b68ca1fcd9d2d8242f
wvdh <wanda@vandenhoogen.eu>
2024-04-09 23:12:29 +0200

    changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
-let firstCard = 10
-let secondCard = 4
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 let cards = [firstCard, secondCard] // array - ordered list of items
 let sum = firstCard + secondCard
 let hasBlackJack = false
@@ -10,14 +10,22 @@ let message = ""
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+// Make this function return a random number between 1 and 13
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random() * 11 ) + 1
+    return randomNumber
+}
+
 function startGame() {
     renderGame()    
 }
 
 function renderGame() {
-    // render out firstCard and secondCard
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
-    // render out ALL the cards we have
+    cardsEl.textContent = "Cards: "
+    // Create a for loop that renders out all the cards instead of just two
+    for ( let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -32,7 +40,7 @@ function renderGame() {
 }
 
 function newCard() {
-    let card = 6
+    let card = getRandomCard()
     sum += card
     // Push the card to the cards array
     cards.push(card)
