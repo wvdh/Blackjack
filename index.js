17592af00018f11ccb23c5d7675550775ba8f75a
wvdh <wanda@vandenhoogen.eu>
2024-04-10 22:33:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function renderGame() {
 
 function newCard() {
     // Only allow the player to get a new card if she IS alive and does NOT have Blackjack
-    if (hasBlackJack === false && isAlive === true) {
+    if (isAlive === true && hasBlackJack === false) {
         let card = getRandomCard()
         sum += card
         cards.push(card)
